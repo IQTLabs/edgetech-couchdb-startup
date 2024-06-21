6360bc483dde8580920f1a09ea896c07fdbcb4f7
Mona Gogia <mgogia@iqt.org>
2024-06-21 17:41:10 +0000

    Parameterize DB name and upgrade couchdb

--- couchdb-startup/couchdbinit.sh
+++ couchdb-startup/couchdbinit.sh
@@ -3,12 +3,12 @@
 # Wait for the CouchDB instance container to be ready before executing setup commands
 while [ "$(curl -s -o /dev/null -w ''%{http_code}'' couchdb-server:5984)" != "200" ]; do sleep 5; done
 
-# Create system tables and table "aisonobuoy" which is used for synching all data from devices
+# Create system tables and table "multimodal" which is used for synching all data from devices
 # TODO: Do not hardcode CouchDB server IP?
 curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/_users
 curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/_replicator
 curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/_global_changes
-curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/aisonobuoy
+curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/${DB_NAME}
 curl -X GET http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/_all_dbs
 
 # In case the container is being on a mobile device,
@@ -16,6 +16,6 @@ curl -X GET http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/_all_
 
 if ${PERIPHERAL}; then
 
-    curl -X POST http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/_replicator -d '{"source":"http://'${COUCHDB_USER}':'${COUCHDB_PASSWORD}'@couchdb-server:5984/aisonobuoy", "target":"http://'${COUCHDB_USER}':'${COUCHDB_PASSWORD}'@'${COUCHDB_SERVER_IP_ADDR}':5984/aisonobuoy", "continuous":true}' -H "Content-Type: application/json"
+    curl -X POST http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb-server:5984/_replicator -d '{"source":"http://'${COUCHDB_USER}':'${COUCHDB_PASSWORD}'@couchdb-server:5984/multimodal", "target":"http://'${COUCHDB_USER}':'${COUCHDB_PASSWORD}'@'${COUCHDB_SERVER_IP_ADDR}':5984/multimodal", "continuous":true}' -H "Content-Type: application/json"
 
 fi
